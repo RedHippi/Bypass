ab1eebc602ade323448f774277f952d33b609eeb
Malek <maleka@alumni.cmu.edu>
2020-02-11 22:37:54 +0300

    Adding improved grid-snapping, prevented icon stacking

--- Assets/Browser Scripts/IconManager.cs
+++ Assets/Browser Scripts/IconManager.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class IconManager : MonoBehaviour
@@ -14,61 +11,38 @@ public class IconManager : MonoBehaviour
     private GameObject icon;
 
     [SerializeField]
-    private GameObject Window;
-    private RectTransform CanvasRect;
-    //Which multiple to snap to on the desktop
-    [SerializeField]
-    private int gridX = 50;
-    [SerializeField]
-    private int gridY = 50;
-    [SerializeField]
-    private int offsetX = 150;
-    [SerializeField]
-    private int offsetY = 150;
+    private GameObject windowPrefab;
+    private IconLayoutManager layout;
+    private Vector2 initialPos;
+    private bool dragging = false;
 
     void Start()
     {
         icon = this.transform.gameObject;
-        CanvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        layout = this.transform.parent.GetComponent<IconLayoutManager>();
+        //Assert.IsNotNull(layout);
     }
 
-    private float roundToMultiple(float x, float multiple)
-    {
-        return Mathf.Round((x / multiple)) * multiple;
-    }
-
-    //Corners are bottom left, top left, top right, and bottom right respec.
-    private bool InBounds(Vector3[] corners, Vector2 mousePos)
-    {
-        if(corners[0].x <= mousePos.x && corners[0].y <= mousePos.y &&
-            corners[1].x <= mousePos.x && corners[1].y >= mousePos.y &&
-            corners[2].x >= mousePos.x && corners[2].y >= mousePos.y &&
-            corners[3].x >= mousePos.x && corners[3].y <= mousePos.y)
-        {
-            return true;
-        }
-
-        return false;
-    }
 
     public virtual void OnPointerClick(PointerEventData eventData)
     {
-        
+        //TODO: Highlight if one click.
+
         if (eventData.clickCount == 2)
         {
             //Doesn't quite work; problems with scaling. But good for now.
             GameObject canvas = this.transform.parent.gameObject;
             Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2);
-            GameObject newWindow = Instantiate(Window, center, Quaternion.identity);
+            GameObject newWindow = Instantiate(windowPrefab, center, Quaternion.identity);
             newWindow.transform.SetParent(canvas.transform);
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector3[] corners = new Vector3[4];
-        CanvasRect.GetWorldCorners(corners);
-        if(InBounds(corners, eventData.position))
+        //To reset to a known position in case "something" happens
+        if(!dragging) { dragging = true; initialPos = this.transform.position; }
+        if(layout.InBounds(eventData.position))
         {
             icon.transform.position = eventData.position;
         }
@@ -76,21 +50,8 @@ public class IconManager : MonoBehaviour
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Vector3[] corners = new Vector3[4];
-        CanvasRect.GetWorldCorners(corners);
-
-        float leftX = corners[0].x; float rightX = corners[2].x;
-        float botY = corners[0].y; float topY = corners[2].y;
-
-        Vector2 currentPos = new Vector2(roundToMultiple(eventData.position.x, gridX),
-                                         roundToMultiple(eventData.position.y, gridY));
-
-        if(currentPos.x < leftX + offsetX) { currentPos.x = leftX + offsetX; }
-        if(currentPos.x > rightX - offsetX) { currentPos.x = rightX - offsetX; }
-        if(currentPos.y < botY + offsetY) { currentPos.y = botY + offsetY; }
-        if(currentPos.y > topY - offsetY) { currentPos.y = topY - offsetY; }
-
-        icon.transform.position = currentPos;
+        dragging = false;
+        icon.transform.position = layout.CheckEndPosition(initialPos, eventData.position);
     }
 
 }
