039782fa7690ba7a29c2a07e2a5907c4159c4ec6
Malek <maleka@alumni.cmu.edu>
2020-02-14 08:41:17 +0300

    Pages now load in "chunks"

--- Assets/Browser Scripts/BrowserManager.cs
+++ Assets/Browser Scripts/BrowserManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Assertions;
 
 [Serializable]
 public class URLPair
@@ -27,18 +28,25 @@ public class BrowserManager : MonoBehaviour
     [Header("Site Stuff")]
     private List<URLPair> sites;
     [SerializeField]
-    private float minLoadingTime = 0;
-    [SerializeField]
-    private float maxLoadingTime = 0;
+    [Tooltip("Left value is the minimum time, right is the maximum.")]
+    private Vector2 totalLoadingRange;
     [SerializeField]
     private GameObject loadingIcon;
 
     private Dictionary<string, GameObject> sitesDic;
     private ScrollRect scrollComp;
     private GameObject currentlyActive; //Should always be only one
+    private float minPageLoadingTime;
+    private float maxPageLoadingTime;
+    private Vector2 contentStartPosition;
 
     void Start()
     {
+        minPageLoadingTime = totalLoadingRange.x;
+        maxPageLoadingTime = totalLoadingRange.y;
+        Assert.IsTrue(minPageLoadingTime < maxPageLoadingTime);
+        Assert.IsTrue(minPageLoadingTime >= 0 && maxPageLoadingTime >= 0);
+
         scrollComp = this.transform.GetComponent<ScrollRect>();
         sitesDic = new Dictionary<string, GameObject>();
 
@@ -54,33 +62,77 @@ public class BrowserManager : MonoBehaviour
         DisplayPageQuick(homePage);
     }
 
+    //I want the load time per element on a website to be random, so
+    //this function generates a wait period per element. 
+    void GenerateRandomSplit(float target, float[] output)
+    {
+        float total = 0;
+        for(int i = 0; i < output.Length; i++)
+        {
+            output[i] = UnityEngine.Random.Range(0.0f, 1.0f);
+            total += output[i];
+        }
+
+        //Scaling
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = (output[i] / total) * target;
+            if(i != 0) { output[i] += output[i - 1]; }
+        }
+    }
+
+    void DeactivateChildren(GameObject obj)
+    {
+        for(int i = 0; i < obj.transform.childCount; i++)
+        {
+            obj.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
     IEnumerator DisplayPage(GameObject obj)
     {
-        float loadTime = UnityEngine.Random.Range(minLoadingTime, maxLoadingTime);
-        float passedTime = 0;
+        float loadTime = UnityEngine.Random.Range(minPageLoadingTime, maxPageLoadingTime);
+        float totalTime = 0;
+        int currentIndex = 0;
         loadingIcon.SetActive(true);
+        obj.SetActive(true);
+        float[] waitArr = new float[obj.transform.childCount]; //child count > 0
+        DeactivateChildren(obj);
+        GenerateRandomSplit(loadTime, waitArr);
 
-        while (passedTime < loadTime)
+        while (totalTime < loadTime)
         {
-            passedTime += Time.deltaTime;
+            totalTime += Time.deltaTime;
+            if(totalTime > waitArr[currentIndex])
+            {
+                obj.transform.GetChild(currentIndex).gameObject.SetActive(true);
+                currentIndex++;
+            }
             yield return null;
         }
 
         loadingIcon.SetActive(false);
-        if (currentlyActive) { currentlyActive.SetActive(false); }
-        obj.SetActive(true);
+        if (currentlyActive)
+        {
+            currentlyActive.GetComponent<RectTransform>().anchoredPosition = contentStartPosition;
+            currentlyActive.SetActive(false);
+        }
         currentlyActive = obj;
         scrollComp.content = obj.GetComponent<RectTransform>();
-
+        contentStartPosition = obj.GetComponent<RectTransform>().anchoredPosition;
         yield break;
     }
 
     void DisplayPageQuick(GameObject obj)
     {
-        if (currentlyActive) { currentlyActive.SetActive(false); }
+        if (currentlyActive) {
+            currentlyActive.GetComponent<RectTransform>().anchoredPosition = contentStartPosition;
+            currentlyActive.SetActive(false);
+        }
         obj.SetActive(true);
         currentlyActive = obj;
         scrollComp.content = obj.GetComponent<RectTransform>();
+        contentStartPosition = obj.GetComponent<RectTransform>().anchoredPosition;
     }
 
     public void CheckURL(string url)
@@ -94,7 +146,4 @@ public class BrowserManager : MonoBehaviour
             DisplayPageQuick(error404);
         }
     }
-
-    //IDEAS: Add a loading bar and some buffer "loading time" to make it look better?
-    //IDEAS: Make the page load in chunks, like back in the day
 }
