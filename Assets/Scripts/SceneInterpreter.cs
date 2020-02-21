53b57a772f9b83be5d6a348b5b8195f06467a383
RedHippi <32603448+RedHippi@users.noreply.github.com>
2020-02-21 12:27:05 -0500

    Working on Silly Stuff
    Working on the ability to look into a different scene.

---
+++ Assets/Scripts/SceneInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneInterpreter : MonoBehaviour
+{
+    public string MyGame;
+    private Camera Subject;
+
+
+    // On Start, we are opening our Game
+    void Start()
+    {
+        //SceneManager.LoadScene(MyGame,LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Test()//TODO: Get camera to map its view to the image.
+    {
+        SceneManager.LoadScene(MyGame, LoadSceneMode.Additive);
+        GameObject[] g = SceneManager.GetSceneByName(MyGame).GetRootGameObjects();
+        Subject = g[0].GetComponent<Camera>();
+       // gameObject.GetComponent<Image>().material = 
+
+    
+}
