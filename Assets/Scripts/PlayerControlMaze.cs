29bc18e8a368aae48a374d44c5468a5be59ae6cc
auhsoja <jabrams.nc@gmail.com>
2020-02-22 19:03:32 -0500

    First steps towards making the maze minigame

--- Assets/Scripts/PlayerControlMaze.cs
+++ Assets/Scripts/PlayerControlMaze.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControlMaze : MonoBehaviour
 {
     public float speed = 5f;
 
@@ -16,13 +16,18 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+
+        transform.Translate(horz * speed * Time.deltaTime, vert * speed * Time.deltaTime, 0);
+        /*
         //Move Left
         if(Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
         //Move Right
-        if (Input.GetKey(KeyCode.D)) { /*Move Right*/}
+        if (Input.GetKey(KeyCode.D)) { /*Move Right} */
 
         //Jump
         if (Input.GetKey(KeyCode.Space) && CanJump) { /*Do a full routine */}
