3cb1309d36822f94ff5c37398454e7ab37e29f82
Julia Shuieh <crystallineblade@gmail.com>
2020-02-23 14:18:11 -0500

    initial commit

---
+++ Assets/Scripts/Text File Scripts/TextWindowScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextWindowScript : WindowScript
+{
+
+    void Start()
+    {
+    }
+
+    public override void ReadInput(GameObject g)
+    {
+        if (g.name == "Mini") { IconParent.GetComponent<TextIconScript>().ToggleMin(); } //Run minimize on IconParent
+        if (g.name == "Close") { IconParent.GetComponent<TextIconScript>().CloseWindow(); }
+    }
+}
