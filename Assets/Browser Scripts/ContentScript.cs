bcddb351473e92386a241c4a41f565fcbcafd1b9
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-02-23 17:26:35 -0500

    added folder UI

---
+++ Assets/Browser Scripts/ContentScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentScript : MonoBehaviour
+{
+    
+    public GameObject Home;
+    public GameObject Downloads;
+    public GameObject Documents;
+    public GameObject Desktop;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void toggleHome(){
+        Home.SetActive (!Home.activeSelf);
+    }
+    public void toggleDownloads(){
+        Downloads.SetActive (!Downloads.activeSelf);
+    }
+    public void toggleDocuments(){
+        Documents.SetActive (!Documents.activeSelf);
+    }
+    public void toggleDesktop(){
+        Desktop.SetActive (!Desktop.activeSelf);
+    }
+}
