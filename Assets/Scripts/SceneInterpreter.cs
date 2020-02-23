4b94d739dac0b1741029c8aac590b5c816547280
RedHippi <32603448+RedHippi@users.noreply.github.com>
2020-02-22 21:38:01 -0500

    My Face is in the scene
    Great! I'll go grab my stuff!!!

--- Assets/Scripts/SceneInterpreter.cs
+++ Assets/Scripts/SceneInterpreter.cs
@@ -7,27 +7,26 @@ using UnityEngine.UI;
 public class SceneInterpreter : MonoBehaviour
 {
     public string MyGame;
+    public TexturePasser tp;
+    public int index;
     private Camera Subject;
 
-
     // On Start, we are opening our Game
-    void Start()
+    void Start()//TODO: Get camera to map its view to the image.
     {
+        
         //SceneManager.LoadScene(MyGame,LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public void Test()//TODO: Get camera to map its view to the image.
+    public void Test()
     {
         SceneManager.LoadScene(MyGame, LoadSceneMode.Additive);
-        GameObject[] g = SceneManager.GetSceneByName(MyGame).GetRootGameObjects();
-        Subject = g[0].GetComponent<Camera>();
-       // gameObject.GetComponent<Image>().material = 
+    }
 
     
 }
