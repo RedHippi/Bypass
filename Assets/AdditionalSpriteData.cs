629f237115ba5903bf70dd2f6d3bbd74c280f64f
RedHippi <32603448+RedHippi@users.noreply.github.com>
2020-06-11 21:24:24 -0400

    Working towards nightlife integration
    -Map text file is built.
    
    -Images are incorrectly set at the moment. Hacky fix is too hard so we are waiting for the artiiist

---
+++ Assets/AdditionalSpriteData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditionalSpriteData : MonoBehaviour
+{
+    public float X;
+    public float Y;
+
+
+    public void LoadBounds()
+    {
+        SpriteRenderer mine = gameObject.GetComponent<SpriteRenderer>();
+        Bounds tmp = mine.bounds;
+        tmp.size = new Vector3(X,Y, 0f);
+    }
+
+    public void PrintBounds()
+    {
+        Vector3 tmp = gameObject.GetComponent<SpriteRenderer>().bounds.size;
+        Debug.Log($"{gameObject.name}'s bounds are [X:{tmp.x}|Y:{tmp.y}]");
+    }
+}
---
+++ Assets/Editor/SpriteDataEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AdditionalSpriteData)),CanEditMultipleObjects]
+public class NewBehaviourScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AdditionalSpriteData Mine = (AdditionalSpriteData)target;
+        if (GUILayout.Button("Set Bounds"))
+        {
+            Mine.LoadBounds();
+        }
+        if (GUILayout.Button("Print Bounds"))
+        {
+            Mine.PrintBounds();
+        }
+    }
+}
